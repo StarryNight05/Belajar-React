1ff9dd6aa2ea174acbb36dc7d37137ab24480466
Bryant1356 <bryantv0.2.0@gmail.com>
2023-05-23 11:06:53 +0700

    save bentar wifi lag jir

--- backend/controllers/Product.js
+++ backend/controllers/Product.js
@@ -1,13 +1,45 @@
 import Products from "../models/ProductModel.js";
+import Users from "../models/UserModel.js";
 
-export const getProducts = (req, res) => {
-
+export const getProducts = async (req, res) => {
+    try {
+        let response;
+        if (req.role === 'admin') {
+            response = await Products.findAll({
+                include: [{
+                    model: Users
+                }]
+            });
+        } else {
+            response = await Products.findAll({
+                where: {
+                    userId: req.userId
+                },
+                include: [{
+                    model: Users
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
 }
 export const getProductById = (req, res) => {
 
 }
-export const createProduct = (req, res) => {
-
+export const createProduct = async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        await Products.create({
+            name: name,
+            price: price,
+            userId: req.userId
+        });
+        res.status(201).json({ msg: "Produk berhasil ditambah" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
 }
 export const updateProduct = (req, res) => {
 
