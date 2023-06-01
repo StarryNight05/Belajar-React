bc5cc9eb2de22de42e0fa65e0d43f5997b66167a
Bryant1356 <bryantv0.2.0@gmail.com>
2023-06-01 16:14:43 +0700

    gnati tampilan login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -24,26 +24,44 @@ const Login = () => {
     }
 
     return (
-        <section className='mt-5'>
+        <section style={{ marginTop: "245px" }}>
             <div className='container'>
-                <div className="row justify-content-center">
-                    <div className="col-md-4">
-                        <form onSubmit={Auth} className='card'>
-                            {isError && <p className='text-center'>{message}</p>}
-                            <div className="card-body">
-                                <div className="d-flex justify-content-center">
-                                    <h1 className="title fw-bold">Login</h1>
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-8 card shadow">
+                        {isError &&
+                            <div class="alert alert-danger" role="alert">
+                                <div className="d-flex justify-content-between">
+                                    <strong>{message}</strong>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                                 </div>
-                                <div className="form-group">
-                                    <label className='fw-bold'><BiMailSend></BiMailSend>&nbsp;Email :</label>
-                                    <input type="text" className='form-control' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
+                            </div>
+                        }
+                        <form onSubmit={Auth}>
+                            <div className="row">
+                                <div className="col-md-7 bg-primary">
+                                    <div className="tagline">
+                                        <div className="d-flex justify-content-center">
+                                            <h4 className="fw-bold mt-5 text-light">Login</h4>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className="form-group mt-3">
-                                    <label className='fw-bold'><BiLock></BiLock>&nbsp;Password :</label>
-                                    <input type="password" className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='*****' required />
-                                </div>
-                                <div className="form-group mt-3 d-flex justify-content-center">
-                                    <button type="submit" className='btn btn-primary fw-bold text-light' style={{ width: "200px" }}><BiLogIn></BiLogIn>&nbsp;{isLoading ? 'Loading...' : 'Login'}</button>
+                                <div className="col-md-4 ms-5">
+                                    <div className="card-body">
+                                        <div className="d-flex justify-content-center">
+                                            <h1 className="title fw-bold">Login</h1>
+                                        </div>
+                                        <div className="form-group">
+                                            <label className='fw-bold'><BiMailSend></BiMailSend>&nbsp;Email :</label>
+                                            <input type="text" className='form-control' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' required />
+                                        </div>
+                                        <div className="form-group mt-3">
+                                            <label className='fw-bold'><BiLock></BiLock>&nbsp;Password :</label>
+                                            <input type="password" className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='*****' required />
+                                        </div>
+                                        <div className="form-group mt-3 d-flex justify-content-center">
+                                            <button type="submit" className='btn btn-primary fw-bold text-light' style={{ width: "200px" }}><BiLogIn></BiLogIn>&nbsp;{isLoading ? 'Loading...' : 'Login'}</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </form>
